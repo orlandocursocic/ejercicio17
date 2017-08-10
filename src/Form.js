ea44ba7cea6d409298903b992dcd04e604904173
Orlando <orlandocursocic@gmail.com>
2017-08-10 16:29:51 +0200

    Solucionado fallos en las validaciones del formulario

--- src/Form.js
+++ src/Form.js
@@ -11,83 +11,14 @@ class InputText extends Component {
 
 
 class Formulario extends Component {
-	constructor(props) {
-		super(props);
+	constructor() {
+			super();
 
-		this.state = {
-			contact: {},
-			formErrors: {nombre: '', apellidos: '', edad: ''},
-		    nombreValid: false,
-		    apellidosValid: false,
-		    edadValid: false,
-		    formValid: false
-		};
-
-		this.handleNombre = this.handleNombre.bind(this);
-		this.handleApellidos = this.handleApellidos.bind(this);
-		this.handleEdad = this.handleEdad.bind(this);
-
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-
-
-	handleNombre(event){
-	    const contact = this.state.contact;
-	    const value = event.target.value;
-	   	contact.nombre =  value;
-	    this.setState({contact: contact}, () => { this.validateField(contact.nombre, value)});;
-	}
-
-
-	handleApellidos(event){
-	    const contact = this.state.contact;
-	    const value = event.target.value;
-	   	contact.apellidos =  value;
-	    this.setState({contact: contact}, () => { this.validateField(contact.apellidos, value)});
-	}
-
-
-	handleEdad(event){
-	    const contact = this.state.contact;
-	    const value = event.target.value;
-	   	contact.edad =  value;
-	    this.setState({contact: contact}, () => { this.validateField(contact.edad, value)});
-	}
-
-
-	validateField(fieldName, value) {
-	  let fieldValidationErrors = this.state.formErrors;
-	  let nombreValid = this.state.nombreValid;
-	  let apellidosValid = this.state.apellidoValid;
-	  let edadValid = this.state.edadValid;
-
-	  switch(fieldName) {
-	    case 'nombre':
-	      nombreValid = value.length <= 20;
-	      fieldValidationErrors.nombre = nombreValid ? '' : ' is too short';
-	      break;
-	    case 'apellidos':
-	      apellidosValid = value.length <= 20;
-	      fieldValidationErrors.apellidos = apellidosValid ? '': ' is too short';
-	      break;
-	    case 'edad':
-	      edadValid = value.match(/^\d+$/);
-	      fieldValidationErrors.edad = edadValid ? '': ' is not a number';
-	      break;
-	    default:
-	      break;
-	  }
-	  this.setState({formErrors: fieldValidationErrors,
-	                  nombreValid: nombreValid,
-	                  apellidosValid: apellidosValid,
-	                  edadValid: edadValid
-	                }, this.validateForm);
-	}
-
-
-
-	validateForm() {
-	  this.setState({formValid: this.state.nombreValid && this.state.apellidosValid && this.state.edadValid});
+			this.state = {
+				nombre: '',
+				apellidos : '',
+				edad: '',
+		}
 	}
 
 
@@ -96,29 +27,55 @@ class Formulario extends Component {
 		this.state = {value: ''};
 	}
 
+	checkFormulario = (evt) =>{
+		switch (evt.target.name) {
+			case "nombre":
+			var nombre = evt.target.value;
+			if(nombre.length <= 20){
+
+				this.setState({nombre:nombre,apellidos:this.state.apellidos,edad:this.state.edad});
+			}				
+			break;
+			case "apellidos":
+			var apellidos = evt.target.value;
+			if(apellidos.length <= 20){
+
+				this.setState({name:this.state.name, apellidos:apellidos, edad:this.state.edad});
+			}
+			case "edad":
+			var edad = evt.target.value;
+			if(edad>=0 && edad<=150){
+
+				this.setState({name:this.state.name, apellidos:this.state.apellidos, edad:edad});
+			}
+			default:
+			// code...
+			break;
+		}
+	}
+
+
 
 	render(){
 		return (
-		<div>
-			<form onSubmit={this.handleSubmit}>
-	        	<label>
-	        		Nombre: 
-		          	<input name="nombre" type="text" value={this.state.contact.nombre} onChange={this.handleNombre} />
-	        	</label>
-	        	<br/>
-	        	<label>
-		          	Apellidos:
-		          	<input name="apellidos" type="text" value={this.state.contact.apellidos} onChange={this.handleApellidos} />
-	        	</label>
-	        	<br/>
-	        	<label>
-		          	Edad:
-		          	<input name="edad" type="text" value={this.state.contact.edad} onChange={this.handleEdad} />
-	        	</label>
-	        	<br/>
-				<input type="submit" value="Submit" disabled={!this.state.formValid} />
-	      </form>
-      </div>
+		<form onSubmit={this.handleSubmit}>
+        	<label>
+        		Nombre: 
+	          	<input name="nombre" type="text" value={this.state.nombre} onInput={this.checkFormulario} />
+        	</label>
+        	<br/>
+        	<label>
+	          	Apellidos:
+	          	<input name="apellidos" type="text" value={this.state.apellidos} onInput={this.checkFormulario} />
+        	</label>
+        	<br/>
+        	<label>
+	          	Edad:
+	          	<input name="edad" type="text" value={this.state.edad} onInput={this.checkFormulario} />
+        	</label>
+        	<br/>
+			<input type="submit" value="Submit" />
+      </form>
 	);
 	}
 }
